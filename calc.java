4379ee4150aaac348c854efa55d0e89c7b8c25ff
adarsh <01fe22bca014@kletech.ac.in>
2024-02-19 14:28:31 +0530

    This is calc

---
+++ calc.java
@@ -0,0 +1,38 @@
+class calc extends extend_cal
+{
+    public float Sum(int x,int y)
+    {
+        return x + y;
+    }
+    public float Diff(int x,int y)
+    {
+        return x - y;
+    }
+    public float Prod(int x,int y)
+    {
+        return x * y;
+    }
+    public float Div(int x,int y)
+    {
+        return x / y;
+    }
+}
+
+class calculator
+{
+    public static void main(String args[])
+    {
+        float num1=15;
+        float num2=30;
+    calc a = new calc();
+    System.out.println("Sum is:" +a.Sum(2,6));
+    System.out.println("Difference is:" +a.Diff(2,3));
+    System.out.println("Product is:" +a.Prod(2,9));
+    System.out.println("Remainder is:" +a.Div(2,7));
+    double squre =a.squre(num1);
+    System.out.println("Squre of Number  is :" +squre);
+     double cube =a.cube(num1);
+    System.out.println("Cube Number is :"+cube);
+    
+    }
+}
